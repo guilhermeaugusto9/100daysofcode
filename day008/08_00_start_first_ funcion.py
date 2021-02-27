c726d7499d1eadda5de5a410a7170a1298a4bae0
Guilherme Augusto <guilherme.augusto9@outlook.com>
2021-02-26 23:09:34 -0300

    day8

---
+++ day008/08_00_start_first_ funcion.py
@@ -0,0 +1,27 @@
+def greet():
+	print("Hello")
+	print("how are you")
+	print("Isn't the weather nice today?")
+	
+greet()
+
+
+
+# Function that allows for input
+
+def greet_with_name(name ):
+	print(f"Hello {name}")
+	print(f"How do you do {name}")
+	
+greet_with_name("Guilherme")
+
+
+# Function with nmore then 1 input
+
+def greet_with_two(name,location):
+	print(f"Hello {name}")
+	print(f"What is like in {location} ?")
+
+greet_with_two("Jack Bauer","London")
+
+
